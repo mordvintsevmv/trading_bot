a2a57535d1e1ab0a88f45b20d68af4660b3d393d
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-06-07 14:04:43 +0300

    Исправление

--- config/personal_data.py
+++ config/personal_data.py
@@ -1,36 +1,38 @@
 import os
 import sqlite3 as sl
 from dotenv import load_dotenv
-
+from config.crypto_rsa import decrypt
 
 """
     Все персональные данные хранятся в файле .env для обеспечения безопасности 
 """
 
 
-load_dotenv()
-
 def get_token(user_id):
-    conn = sl.connect("db/users.db")
-    cur = conn.cursor()
+    connection = sl.connect("db/BotDB.db")
+    cursor = connection.cursor()
 
-    token = cur.execute('SELECT token FROM USER WHERE id = ? ', (user_id,)).fetchone()[0]
-    return token
+    token = cursor.execute('SELECT token FROM users WHERE user_id = ? ', (user_id,)).fetchone()[0]
+    return decrypt(token)
 
 
 def get_account(user_id):
-    conn = sl.connect("db/users.db")
-    cur = conn.cursor()
+    connection = sl.connect("db/BotDB.db")
+    cursor = connection.cursor()
 
-    account_id = cur.execute('SELECT account_id FROM USER WHERE id = ? ', (user_id,)).fetchone()[0]
+    account_id = cursor.execute('SELECT account_id FROM users WHERE user_id = ? ', (user_id,)).fetchone()[0]
     return account_id
 
-def get_account_type(user_id):
-    conn = sl.connect("db/users.db")
-    cur = conn.cursor()
 
-    account_type = cur.execute('SELECT account_type FROM USER WHERE id = ? ', (user_id,)).fetchone()[0]
+def get_account_type(user_id):
+    connection = sl.connect("db/BotDB.db")
+    cursor = connection.cursor()
+
+    account_type = cursor.execute('SELECT account_type FROM users WHERE user_id = ?', (user_id,)).fetchone()[0]
     return account_type
+
+
+load_dotenv()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = os.environ.get("ADMIN_ID")
