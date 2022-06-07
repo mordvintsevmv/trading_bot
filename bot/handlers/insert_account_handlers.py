9dd97d87014438b2aed35d2d700a2901c0ea5a87
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-06-07 15:20:48 +0300

    Исправление

--- bot/handlers/insert_account_handlers.py
+++ bot/handlers/insert_account_handlers.py
@@ -8,7 +8,8 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, InlineKeyboa
 import sqlite3 as sl
 import os
 from tinkoff.invest import Client
-
+from config.personal_data import get_token
+from config.crypto_rsa import encrypt
 """
 
     Тут представлены все хэндлеры, которые отвечают за добавление токена
@@ -27,7 +28,7 @@ async def choose_account(message: Message):
     conn = sl.connect("db/users.db")
     cur = conn.cursor()
 
-    token = cur.execute('SELECT token FROM USER WHERE id = ? ', (message.from_user.id,)).fetchone()[0]
+    token = get_token(message.from_user.id)
 
     choose_account = InlineKeyboardMarkup()
 
@@ -35,7 +36,8 @@ async def choose_account(message: Message):
         acc = client.users.get_accounts()
         acc_sand = client.sandbox.get_sandbox_accounts()
         for i in acc.accounts:
-            choose_account.add(InlineKeyboardButton(text=f"{i.name}", callback_data=f"account:{i.id}:{i.name}"))
+            if i.name != "Инвесткопилка":
+                choose_account.add(InlineKeyboardButton(text=f"{i.name}", callback_data=f"account:{i.id}:{i.name}"))
         for i in acc_sand.accounts:
             choose_account.add(InlineKeyboardButton(text=f"Песочница", callback_data=f"account:{i.id}:sandbox"))
 
@@ -53,11 +55,23 @@ async def account_finish(callback_query):
     account_id = data[1]
     account_type = data[2]
 
-    conn = sl.connect("db/users.db")
-    cur = conn.cursor()
+    conn_users = sl.connect("db/users.db")
+    cur_users = conn_users.cursor()
+    cur_users.execute('UPDATE USER SET account_id = ?, account_type = ? WHERE user_id = ?;', (account_id, account_type, callback_query.from_user.id))
+    conn_users.commit()
 
-    cur.execute('UPDATE USER SET account_id = ?, account_type = ? WHERE id = ?;', (account_id, account_type, callback_query.from_user.id))
-    conn.commit()
+
+    conn_accounts = sl.connect("db/accounts.db")
+    cur_accounts = conn_accounts.cursor()
+
+    token = encrypt(get_token(callback_query.from_user.id))
+
+    info = cur_accounts.execute('SELECT * FROM ACCOUNTS WHERE user_id = ? and account_id = ?', (callback_query.from_user.id, account_id)).fetchone()
+
+    if info is None:
+        cur_accounts.execute('INSERT INTO ACCOUNTS (user_id, token, account_id, account_type) VALUES (?,?,?,?);',
+                      (callback_query.from_user.id, token, account_id, account_type))
+        conn_accounts.commit()
 
     await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
     await bot.send_message(chat_id=callback_query.from_user.id, text=f"Аккаунт успешно изменён!",reply_markup=get_start_menu(user_id=callback_query.from_user.id))
