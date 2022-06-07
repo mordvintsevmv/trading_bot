5ec1945c867fd7fd5f354efa36bcbcbbb1cd4200
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-06-07 14:01:46 +0300

    Исправление

--- trading/add_money_sandbox.py
+++ trading/add_money_sandbox.py
@@ -1,20 +1,21 @@
-from tinkoff.invest import Client, Quotation, CandleInterval
-from trading.trade_help import total_quantity, to_quotation
-from datetime import datetime, timedelta
-import pandas as pd
-from config.personal_data import get_token, get_account, get_account_type
+from tinkoff.invest import Client
+from trading.trade_help import to_quotation
+from config.personal_data import get_token, get_account
 from tinkoff.invest import MoneyValue
 
 
-def add_money_sandbox(user_id, sum, cur):
+def add_money_sandbox(user_id, sum, cur, account_id=""):
     with Client(get_token(user_id)) as client:
+
+        if account_id == "":
+            account_id = get_account(user_id=user_id)
 
         units = to_quotation(float(sum)).units
         nano = to_quotation(float(sum)).nano
 
-        client.sandbox.sandbox_pay_in(
-            account_id=get_account(user_id),
+        pay_in = client.sandbox.sandbox_pay_in(
+            account_id=account_id,
             amount=MoneyValue(units=units, nano=nano, currency=f"{cur}")
         )
 
-    return 0
+    return pay_in
