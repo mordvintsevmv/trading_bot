4874411b7e5c39e4c4b444e1eba92ead6650a3ba
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-06-07 16:07:55 +0300

    Исправление

--- bot/handlers/info_handlers.py
+++ bot/handlers/info_handlers.py
@@ -1,15 +1,15 @@
-from trading.get_info import get_all_stat, get_my_order, get_all_currency, get_all_shares
+from trading.get_account_info import get_all_stat, get_my_order, get_all_currency, get_all_securities, get_my_operations
 from main import dp, bot
 from aiogram.types import Message
 from aiogram.dispatcher.filters import Text
-from trading.get_by_figi import sfb_name_by_figi
+from trading.get_securities import security_name_by_figi
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from trading.place_order import cancel_order
-from config.personal_data import get_account_type
+from trading.trade_help import get_currency_sing
 
 """
 
-    Здесь собраны все хендлеры, которые отвечают за вывод информации о счёте
+    Здесь собраны все хэндлеры, которые отвечают за вывод информации о счёте
 
 """
 
@@ -22,28 +22,25 @@ from config.personal_data import get_account_type
 
 @dp.message_handler(Text(contains="баланс", ignore_case=True))
 async def get_balance(message: Message):
-
-    cur_df = get_all_currency(message.from_user.id)
+    currency_df = get_all_currency(message.from_user.id)
 
     await message.answer(f"💸<b>Доступная валюта</b>💸")
 
-    for i in cur_df.index:
+    text = ""
 
-        if cur_df['currency'][i] == "rub":
-            await message.answer(
-                f"{round(cur_df['sum'][i], 2)}₽")
+    for i in currency_df.index:
 
-        elif cur_df['currency'][i] == "usd":
-            await message.answer(
-                f"{round(cur_df['sum'][i], 2)}$")
-
-        elif cur_df['currency'][i] == "eur":
-            await message.answer(
-                f"{round(cur_df['sum'][i], 2)}€")
-
+        if currency_df['exchange_rate'][i] != 0.0 and currency_df['exchange_rate'][i] != 1.0:
+            text += (
+                f"<b>{currency_df['name'][i]}</b>\n"
+                f"{round(currency_df['sum'][i], 2)}{currency_df['sign'][i]} <i>({round(currency_df['sum_in_ruble'][i], 2)}₽)</i>\n\n")
         else:
-            await message.answer(
-                f"{round(cur_df['sum'][i], 2)} {cur_df['currency'][i]}")
+            text += (
+                f"<b>{currency_df['name'][i]}</b>\n"
+                f"{round(currency_df['sum'][i], 2)}{currency_df['sign'][i]}\n\n")
+
+    await message.answer(text=text)
+
 
 """
 
@@ -54,58 +51,57 @@ async def get_balance(message: Message):
 
 @dp.message_handler(Text(contains="бумаги", ignore_case=True))
 async def get_share(message: Message):
+    security_df = get_all_securities(message.from_user.id)
 
-    sh_df = get_all_shares(message.from_user.id)
-
-    empty_portf = True
+    empty_portfolio = True
     await message.answer(f"💼Ценные бумаги в портфеле💼")
 
-    for i in sh_df.index:
+    for i in security_df.index:
 
         inst = ""
         name = ""
 
-        if sh_df['instrument'][i] == "share":
+        if security_df['instrument'][i] == "share":
             inst = "Акции"
-            name = sfb_name_by_figi(sh_df['figi'][i], user_id=message.from_user.id)
-            empty_portf = False
+            name = security_name_by_figi(security_df['figi'][i], user_id=message.from_user.id)
+            empty_portfolio = False
 
-        elif sh_df['instrument'][i] == "bond":
+        elif security_df['instrument'][i] == "bond":
             inst = "Бонды"
-            name = sfb_name_by_figi(sh_df['figi'][i], user_id=message.from_user.id)
-            empty_portf = False
+            name = security_name_by_figi(security_df['figi'][i], user_id=message.from_user.id)
+            empty_portfolio = False
 
-        elif sh_df['instrument'][i] == "etf":
+        elif security_df['instrument'][i] == "etf":
             inst = "ETF"
-            name = sfb_name_by_figi(sh_df['figi'][i], user_id=message.from_user.id)
-            empty_portf = False
+            name = security_name_by_figi(security_df['figi'][i], user_id=message.from_user.id)
+            empty_portfolio = False
 
-        elif sh_df['instrument'][i] == "currency":
+        elif security_df['instrument'][i] == "currency":
             continue
 
-        elif sh_df['instrument'][i] == "future":
+        elif security_df['instrument'][i] == "future":
             inst = "Фьючерсы"
-            name = sfb_name_by_figi(sh_df['figi'][i], user_id=message.from_user.id)
-            empty_portf = False
+            name = security_name_by_figi(security_df['figi'][i], user_id=message.from_user.id)
+            empty_portfolio = False
 
-        if sh_df['exp_yield'][i] >= 0:
-            exp_yield = f"Ожидаемый доход: {round(sh_df['exp_yield'][i], 2)}₽"
+        if security_df['exp_yield'][i] >= 0:
+            exp_yield = f"Ожидаемый доход: {round(security_df['exp_yield'][i], 2)}₽"
         else:
-            exp_yield = f"Ожидаемая убыль: {round(sh_df['exp_yield'][i], 2)}₽"
+            exp_yield = f"Ожидаемая убыль: {round(security_df['exp_yield'][i], 2)}₽"
 
         await message.answer(
             f"🧾<b>{inst} {name}</b>\n"
-            f"FIGI: {sh_df['figi'][i]}\n\n"
-            f"Лотов: {int(sh_df['lots'][i])}\n"
-            f"Всего: {round(sh_df['quantity'][i], 2)}\n\n"
-            f"Средняя цена: {sh_df['average_price'][i]}\n"
-            f"Средняя цена FIFO: {sh_df['average_price_fifo'][i]}\n"
-            f"Текущая цена: {round(sh_df['current_price'][i], 6)}\n\n"
-            f"НКД: {sh_df['nkd'][i]}\n"
+            f"FIGI: {security_df['figi'][i]}\n\n"
+            f"Лотов: {int(security_df['lots'][i])}\n"
+            f"Всего: {round(security_df['quantity'][i], 2)}\n\n"
+            f"Средняя цена: {security_df['average_price'][i]}\n"
+            f"Средняя цена FIFO: {security_df['average_price_fifo'][i]}\n"
+            f"Текущая цена: {round(security_df['current_price'][i], 6)}\n\n"
+            f"НКД: {security_df['nkd'][i]}\n"
             f"{exp_yield}\n"
         )
 
-    if empty_portf:
+    if empty_portfolio:
         await message.answer(f"У Вас нет ценных бумаг в портфеле!")
 
 
@@ -125,11 +121,11 @@ async def get_stat(message: Message):
     # Посчитаем сумму всех бумаг
     sum = stat['sum_total'][0]
 
-    # Переведём доход/убыток из проценты в рубли
+    # Переведём доход/убыток из процентов в рубли
     dif = round(sum - (sum / (100 + stat['exp_yield'][0])) * 100, 2)
 
     if dif >= 0:
-        dif_text = f"<b>Прибль</b>: {dif}₽ ({stat['exp_yield'][0]}%)"
+        dif_text = f"<b>Прибыль</b>: {dif}₽ ({stat['exp_yield'][0]}%)"
     else:
         dif_text = f"<b>Убыль</b>: {dif}₽ ({stat['exp_yield'][0]}%)"
 
@@ -152,65 +148,70 @@ async def get_stat(message: Message):
 
 @dp.message_handler(Text(contains="ордер", ignore_case=True))
 async def get_orders(message: Message):
-    ord_df = get_my_order(message.from_user.id)
+    order_df = get_my_order(message.from_user.id)
 
     await message.answer(f"📋Открытые ордера📋")
 
-    for i in ord_df.index:
+    for i in order_df.index:
 
-        keyboard = InlineKeyboardMarkup(
-            inline_keyboard=
-            [
-                [
-                    InlineKeyboardButton(text=f"Отменить ордер", callback_data=f"ord_id{ord_df['order_id'][i]}"),
-                ]
-            ],
-        )
+        cancel_order_keyboard = InlineKeyboardMarkup()
+        cancel_order_keyboard.add(InlineKeyboardButton(text=f"Отменить ордер", callback_data=f"cancel_order:{order_df['order_id'][i]}"))
 
-        if ord_df['direction'][i] == 2:
-            dir = "Продажа"
+        if order_df['direction'][i] == 2:
+            direction = "Продажа"
         else:
-            dir = "Покупка"
+            direction = "Покупка"
 
-        if ord_df['currency'][i] == "rub":
-            currency = "₽"
-
-        elif ord_df['currency'][i] == "eur":
-            currency = "€"
-
-        elif ord_df['currency'][i] == "usd":
-            currency = "$"
-
-        else:
-            currency = ord_df['currency'][i]
+        currency = get_currency_sing(order_df['currency'][i])
 
         await message.answer(
-            f"✅<b>{dir}</b> бумаг {sfb_name_by_figi(ord_df['figi'][i], message.from_user.id)}\n"
-            f"Открыт: {ord_df['order_date'][i]}\n\n"
-            f"ID: {ord_df['order_id'][i]}\n\n"
-            f"FIGI: {ord_df['figi'][i]}\n\n"
-            f"Лотов запрошено: {ord_df['lots_req'][i]}\n"
-            f"Лотов исполнено: {ord_df['lots_ex'][i]}\n\n"
-            f"Сумма запрошена: {ord_df['sum_req'][i]}{currency}\n"
-            f"Сумма исполнено: {ord_df['sum_ex'][i]}{currency}\n\n"
-            f"Цена одной акции: {round(ord_df['price_one'][i], 6)}{currency}\n\n"
-            f"Комиссия: {round(ord_df['commission'][i], 3)}{currency}\n"
-            f"Комиссия сервиса: {round(ord_df['serv_commission'][i], 3)}{currency}\n\n"
-            f"Итого: {ord_df['sum_total'][i]}{currency}\n",
-            reply_markup=keyboard
+            f"✅<b>{direction}</b> бумаг {security_name_by_figi(order_df['figi'][i], message.from_user.id)}\n"
+            f"Открыт: {order_df['order_date'][i]}\n\n"
+            f"ID: {order_df['order_id'][i]}\n\n"
+            f"FIGI: {order_df['figi'][i]}\n\n"
+            f"Лотов запрошено: {order_df['lots_req'][i]}\n"
+            f"Лотов исполнено: {order_df['lots_ex'][i]}\n\n"
+            f"Сумма запрошена: {order_df['sum_req'][i]}{currency}\n"
+            f"Сумма исполнено: {order_df['sum_ex'][i]}{currency}\n\n"
+            f"Цена одной акции: {round(order_df['price_one'][i], 6)}{currency}\n\n"
+            f"Комиссия: {round(order_df['commission'][i], 3)}{currency}\n"
+            f"Комиссия сервиса: {round(cancel_order_keyboard['serv_commission'][i], 3)}{currency}\n\n"
+            f"Итого: {order_df['sum_total'][i]}{currency}\n",
+            reply_markup=cancel_order_keyboard
         )
 
 
 '''
-    Закртыие ордера по id
+    Закрытие ордера по id
 '''
 
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('ord_id'))
+
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('cancel_order'))
 async def close_order(callback_query):
+    data = callback_query.data.split(":")
+    order_id = data[1]
 
-    order_id = callback_query.data[6:]
+    await cancel_order(order_id=order_id, user_id=callback_query.from_user.id)
 
-    await cancel_order(order_id, user_id=callback_query.from_user.id)
+    await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id,
+                                text="❌Ордер отменён❌")
 
-    await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id, text="❌Ордер отменён❌")
 
+'''
+    Операции
+'''
+
+
+@dp.message_handler(Text(contains="операции", ignore_case=True))
+async def get_operations(message: Message):
+
+    operations = get_my_operations(user_id=message.from_user.id)
+
+    if operations:
+        await message.answer(f"Ваши операции:")
+
+        with open(f"img/operations/all_operations_{message.from_user.id}.png", "rb") as operations_img:
+            await bot.send_photo(chat_id=message.from_user.id, photo=operations_img)
+
+    else:
+        await message.answer(f"У Вас пока не было операций!")
