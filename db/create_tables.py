b8e7d683b1ef697721e4498a00251ff8d24cfaef
Mordvintsevmv <mordvintsevmv@gmail.com>
2022-06-07 14:07:11 +0300

    Исправление

--- db/create_tables.py
+++ db/create_tables.py
@@ -2,50 +2,76 @@ import sqlite3 as sl
 
 
 def create_tables():
-    conn_op = sl.connect("db/operations.db")
+    connection = sl.connect("db/BotDB.db")
 
-    conn_op.execute('CREATE TABLE IF NOT EXISTS OPERATIONS ('
-                    'id INTEGER PRIMARY KEY NOT NULL,'
-                    'user_id TEXT,'
-                    'account_id TEXT,'
-                    'account_type TEXT,'
-                    'order_id TEXT,'
-                    'date_op TEXT,'
-                    'time_op TEXT,'
-                    'operation TEXT,'
-                    'figi TEXT,'
-                    'name TEXT,'
-                    'quantity_lots INTEGER,'
-                    'quantity_total INTEGER,'
-                    'price REAL,'
-                    'price_total REAL,'
-                    'commission REAL,'
-                    'via TEXT)')
+    # Таблица содержит данные обо всех проведённых операциях
+    connection.execute('CREATE TABLE IF NOT EXISTS operations ('
+                       'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+                       'user_id TEXT,'
+                       'account_id TEXT,'
+                       'account_type TEXT,'
+                       'order_id TEXT,'
+                       'date_op TEXT,'
+                       'time_op TEXT,'
+                       'direction TEXT,'
+                       'figi TEXT,'
+                       'ticker TEXT,'
+                       'name TEXT,'
+                       'quantity_lots INTEGER,'
+                       'in_lot INTEGER,'
+                       'quantity_total INTEGER,'
+                       'price_position REAL,'
+                       'price_total REAL,'
+                       'commission REAL,'
+                       'currency TEXT,'
+                       'message TEXT,'
+                       'via TEXT)')
 
-    conn_str1 = sl.connect("db/str1.db")
+    # Таблица содержит данные о конфигурации стратегии EMA + ADX + MACD
+    connection.execute('CREATE TABLE IF NOT EXISTS str1_config ('
+                       'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+                       'user_id TEXT,'
+                       'account_id TEXT,'
+                       'account_type TEXT,'
+                       'figi TEXT,'
+                       'name TEXT,'
+                       'trade_status TEXT,'
+                       'notif_status TEXT,'
+                       'buy_price REAL,'
+                       'currency TEXT,'
+                       'quantity_lots INTEGER,'
+                       'period INTEGER,'
+                       'macd_border REAL,'
+                       'adx_border REAL,'
+                       'take_profit REAL,'
+                       'stop_loss REAL)')
 
-    conn_str1.execute('CREATE TABLE IF NOT EXISTS CONFIG ('
-                      'id INTEGER PRIMARY KEY NOT NULL,'
-                      'user_id TEXT,'
-                      'account_id TEXT,'
-                      'figi TEXT,'
-                      'name TEXT,'
-                      'trade_status TEXT,'
-                      'notif_status TEXT,'
-                      'buy_price REAL,'
-                      'period INTEGER,'
-                      'macd_border REAL,'
-                      'adx_border REAL,'
-                      'take_profit REAL,'
-                      'stop_loss REAL)')
+    # Таблица содержит данные пользователя, которые используются в данный момент
+    connection.execute('CREATE TABLE IF NOT EXISTS users ('
+                       'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+                       'user_id INTEGER NOT NULL,'
+                       'first_name TEXT,'
+                       'last_name TEXT,'
+                       'username TEXT,'
+                       'token TEXT,'
+                       'account_id TEXT,'
+                       'account_type TEXT,'
+                       'access_level TEXT)')
 
-    conn_user = sl.connect("db/users.db")
+    connection_message = sl.connect("db/message.db")
 
-    conn_user.execute('CREATE TABLE IF NOT EXISTS USER ('
-                      'id INTEGER PRIMARY KEY NOT NULL,'
-                      'first_name TEXT,'
-                      'last_name TEXT,'
-                      'username TEXT,'
-                      'token TEXT,'
-                      'account_id TEXT,'
-                      'account_type TEXT)')
+    connection_message.execute('CREATE TABLE IF NOT EXISTS message_type ('
+                               'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+                               'message_type INTEGER NOT NULL,'
+                               'message TEXT)')
+
+    connection_message.execute('CREATE TABLE IF NOT EXISTS messages ('
+                               'id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+                               'message_type INTEGER NOT NULL,'
+                               'user_id INTEGER,'
+                               'account_type TEXT,'
+                               'date_message TEXT,'
+                               'time_message TEXT,'
+                               'function TEXT,'
+                               'user_text TEXT)')
+    return 0
